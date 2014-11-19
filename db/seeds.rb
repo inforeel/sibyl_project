f6a49a9bdc7673825fa77e0c85428168f6904896
inforeel <theinforeel@gmail.com>
2014-11-18 21:42:42 -0500

    Set up first user, beginning work on the user welcome page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:email => "rupert.giles@oxford.edu", :password => "12345", :password_confirmation => "12345")
