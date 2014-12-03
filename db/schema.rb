e96ba18c68b05c27e865401230a965a1f66fcdb6
inforeel <theinforeel@gmail.com>
2014-12-03 18:04:41 -0500

    reverting addition of resources db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202031307) do
+ActiveRecord::Schema.define(version: 20141203020154) do
 
   create_table "requests", force: true do |t|
     t.text     "question"
